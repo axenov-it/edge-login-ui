452681024da6b292f7ab10c92498ac475a9c492b
William Swanson <swansontec@gmail.com>
2021-06-24 16:21:17 -0700

    x (cd packages/edge-login-ui-rn; ./convert.sh)

---
+++ packages/edge-login-ui-rn/src/components/themed/Divider.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { View } from 'react-native'
+import { cacheStyles } from 'react-native-patina'
+
+import { Theme, ThemeProps, withTheme } from '../services/ThemeContext'
+
+interface Props {
+  marginVertical?: number
+}
+
+/**
+ * Renders a horizontal dividing line.
+ */
+const DividerComponent = ({
+  marginVertical = 1.5,
+  theme
+}: Props & ThemeProps) => {
+  const styles = getStyles(theme)
+
+  return (
+    <View
+      style={[styles.divider, { marginVertical: theme.rem(marginVertical) }]}
+    />
+  )
+}
+
+const getStyles = cacheStyles((theme: Theme) => ({
+  divider: {
+    width: '100%',
+    borderBottomWidth: theme.thinLineWidth,
+    borderBottomColor: theme.lineDivider
+  }
+}))
+
+export const Divider = withTheme(DividerComponent)
