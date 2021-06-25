f0c46da1cc0f2f9de0c5c2cd609029e6dedad2fd
William Swanson <swansontec@gmail.com>
2021-06-25 11:13:04 -0700

    Fix localization

--- packages/edge-login-ui-rn/src/common/locales/en_US.ts
+++ packages/edge-login-ui-rn/src/common/locales/en_US.ts
@@ -1,4 +1,4 @@
-const strings = {
+export default {
   app_name_default: 'Edge',
   edge_site: 'https://edge.app/',
   login_button: 'Login',
@@ -249,5 +249,3 @@ const strings = {
     'Please enable notifications for account security. Edge will notify you if other devices attempt to access your account and will allow you to accept or reject access.',
   change_recovery_same_question: 'Cannot use the same 2 recovery questions'
 }
-
-module.exports = strings
